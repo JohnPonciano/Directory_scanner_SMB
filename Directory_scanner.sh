3ef6ce45a24380c4cc75eafaa8f139a0e8c91c32
Jonathan Ponciano <jhow.ponciano@hotmail.com>
2023-08-29 12:30:22 -0300

    adição de features, procurar por nome ou extensão
    adição

--- Directory_scanner.sh
+++ Directory_scanner.sh
@@ -1,1 +1,22 @@
- 
+ #!/bin/bash
+
+# Função para realizar o scan em um diretório SMB
+scan_directory() {
+    smbclient //"$1"/"$2" -U "$3%$4" -c "recurse; lcd $5; prompt OFF; mget $6"
+}
+
+# Solicitar informações ao usuário
+read -p "Digite o endereço do servidor SMB (ex: //servidor/compartilhamento): " server
+read -p "Digite o nome de usuário: " username
+read -s -p "Digite a senha: " password
+echo
+
+read -p "Digite o diretório SMB a ser verificado: " smb_directory
+read -p "Digite o diretório local para salvar os arquivos: " local_directory
+read -p "Digite o nome ou padrão do arquivo a ser procurado: " file_pattern
+
+# Realizar o scan no diretório SMB
+scan_directory "$server" "$smb_directory" "$username" "$password" "$local_directory" "$file_pattern"
+
+echo "Scan concluído. Arquivos encontrados foram salvos em $local_directory"
+
